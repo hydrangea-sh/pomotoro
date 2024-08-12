0277468dd0531ebebdbcf311e2572ec2e19eba4e
hydrangea-sh <elizabethkang@fastmail.com>
2024-08-12 09:28:08 -0500

    formatting and linting
    Signed-off-by: hydrangea-sh <elizabethkang@fastmail.com>

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,21 @@
 import Toro from "@/assets/toro.webp";
 
 function Nav() {
-  return (
-    <nav class="flex justify-center p-6 max-w-2xl mx-auto">
-      <h1 class="text-4xl font-black text-white">
-        <a href="/" class="flex items-center">
-          <img
-            alt="tuna nigiri"
-            draggable={false}
-            src={Toro}
-            class="h-12 w-12 mr-2"
-          />
-          PomoToro
-        </a>
-      </h1>
-    </nav>
-  );
+	return (
+		<nav class="flex justify-center p-6 max-w-2xl mx-auto">
+			<h1 class="text-4xl font-black text-white">
+				<a href="/" class="flex items-center">
+					<img
+						alt="tuna nigiri"
+						draggable={false}
+						src={Toro}
+						class="h-12 w-12 mr-2"
+					/>
+					PomoToro
+				</a>
+			</h1>
+		</nav>
+	);
 }
 
 export default Nav;
