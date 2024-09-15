34b7665d93dca616b373a440ad0a548c13a01424
hydrangea-sh <elizabethkang@fastmail.com>
2024-09-15 14:00:34 -0500

    update biome config, added vite-biome-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import path from "node:path";
 import { defineConfig } from "vite";
+import biomePlugin from "vite-plugin-biome";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-	plugins: [solid()],
+	plugins: [
+		solid(),
+		biomePlugin({
+			biomeCommandBase: "bunx @biomejs/biome",
+			mode: "check",
+			files: ".",
+			applyFixes: true,
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
